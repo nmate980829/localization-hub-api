fbbac22cebe99d8798b6f2b8feddf22cd678077e
Máté Nagy <nmate980829@gmail.com>
2022-05-12 22:14:06 +0200

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ import { WrapInterceptor } from './utils/response-wrapper/wrap.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
-  app.enableCors({ origin: 'http://localhost:3001' });
+  app.enableCors({ origin: process.env.UI_URL });
   app.setGlobalPrefix('api');
   app.enableVersioning({
     //defaultVersion: process.env.API_VERSION
